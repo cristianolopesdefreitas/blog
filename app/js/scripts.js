781b9689e02cf7849f7e505c0f24aadc8813d343
cristianolopesdefreitas <cristianolopesdefreitas@gmail.com>
2017-11-23 01:02:43 -0200

    Mais ajustes

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -10,9 +10,29 @@
         $aside.toggleClass( openedClass, !$aside.hasClass( openedClass ) );
     }
 
+    function scrollToTopToggle() {
+        var scroll = $document.scrollTop();
+
+        $( '.scroll-to-top' ).toggleClass( 'scroll-to-top__show', scroll > 100 );
+    }
+
+    function scrollToTop() {
+        $( 'html, body' ).animate({
+            scrollTop: 0
+        }, 500 );
+    }
+
     function startListeners() {
         $document.on( 'click', '.aside-toggle', function() {
             asideToggle();
+        });
+
+        $document.on( 'scroll', function() {
+            scrollToTopToggle();
+        });
+
+        $document.on( 'click', '.scroll-to-top', function() {
+            scrollToTop();
         });
     }
 
